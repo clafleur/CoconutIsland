88bc17b0a22d8e3ffdcc00ad155a3f2647021069
clafleur <clayton@texsstudio.com>
2016-10-05 15:05:16 -0700

    Created a catch all script.
    Added a catchall script so that we can destroy objects that fall off the
    edge of our world.

---
+++ Assets/Scripts/CatchAll.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatchAll : MonoBehaviour {
+
+    void OnTriggerEnter(Collider a_other) {
+
+        Destroy(a_other.gameObject);
+
+    }
+}
